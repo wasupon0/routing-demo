98ade0508be7d43b5cf0a0df230499d14e14a264
wasupon <wasupon.mail@gmail.com>
2024-02-25 14:26:43 +0900

    Add: custom not found with condition

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { notFound } from "next/navigation";
 
 export default function ReviewDetail({
   params,
 }: {
   params: { reviewId: string; productId: string };
 }) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
+
   return (
     <h1>
       Review {params.reviewId} about product {params.productId}
