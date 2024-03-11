e5e79b8ea5523bf363f938740a517cb31f08fcb8
wasupon <wasupon.mail@gmail.com>
2024-03-11 20:30:18 +0900

    add: error page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { notFound } from "next/navigation";
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default function ReviewDetail({
   params,
 }: {
   params: { reviewId: string; productId: string };
 }) {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error loading review random");
+  }
+
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
